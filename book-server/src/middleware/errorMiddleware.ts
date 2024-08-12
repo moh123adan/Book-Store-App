12230afe9f974785dbf806d6c4b47318223e905c
maxamed <maxamedadan169@gmail.com>
2024-08-12 17:12:49 +0300

    added errorMiddlware

---
+++ book-server/src/middleware/errorMiddleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+
+// Not Found Middleware
+export const notFound = (req: Request, res: Response, next: NextFunction): void => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+};
+
+// Error Handler Middleware
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction): void => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    });
+};
