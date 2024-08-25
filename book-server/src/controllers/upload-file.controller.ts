0af1e7bd1e470722dd1122abf883a1fa6637b031
maxamed <maxamedadan169@gmail.com>
2024-08-25 21:38:31 +0300

    setting up multer

--- book-server/src/controllers/upload-file.controller.ts
+++ book-server/src/controllers/upload-file.controller.ts
@@ -3,3 +3,13 @@ import { initializeApp } from 'firebase/app';
 
 import { getStorage, ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage'
 import multer from 'multer';
+
+import config from '../config/firebase.config';
+
+const router: Router = express.Router();
+
+
+
+
+
+
